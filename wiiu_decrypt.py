a6cb2d8955f11d77fe867c3f023706f27f14ac19
ihaveamac <ian@ianburgwin.net>
2016-12-19 07:43:30 -0800

    finally properly decrypt files with hash tree

--- wiiu_decrypt.py
+++ wiiu_decrypt.py
@@ -33,10 +33,16 @@ if not os.path.isfile("title.tmd"):
     sys.exit("No TMD (title.tmd) was found.")
 
 
-def showprogress(val, maxval):
+def show_progress(val, maxval):
     # crappy workaround I bet, but print() didn't do what I wanted
     minval = min(val, maxval)
     sys.stdout.write("\r- {:>5.1f}% {:>10} / {}".format((minval / maxval) * 100, minval, maxval))
+    sys.stdout.flush()
+
+
+def show_chunk(num, count):
+    # crappy workaround I bet, but print() didn't do what I wanted
+    sys.stdout.write("\r- Chunk {:>5} / {:>5}".format(num + 1, count))
     sys.stdout.flush()
 
 # find title id and content id
@@ -86,31 +92,83 @@ print("Decrypted Titlekey:     " + binascii.hexlify(decrypted_titlekey).decode('
 
 for c in contents:
     print("Decrypting {}...".format(c[0]))
-    cipher_content = AES.new(decrypted_titlekey, AES.MODE_CBC, c[1] + (b"\0" * 14))
-    content_hash = SHA1.new()
     left = os.path.getsize(c[0] + ".app")  # set to current size
 
-    with open(c[0] + ".app", "rb") as encrypted:
-        with open(c[0] + ".app.dec", "wb") as decrypted:
-            for __ in itertools.repeat(0, int(math.floor((c[3] / readsize)) + 1)):
-                to_read = min(readsize, left)
-                tmp_enc = encrypted.read(to_read)
-                tmp_dec = cipher_content.decrypt(tmp_enc)
-                if not c[2] & 0x2:
-                    content_hash.update(tmp_dec)
-                decrypted.write(tmp_dec)
-                left -= readsize
-                showprogress(c[3] - left, c[3])
-                if left <= 0:
-                    print("")
-                    break
-    if c[2] & 0x2:
+    if c[2] & 0x2:  # if has a hash tree
+        chunk_count = left // 0x10000
+        chunk_num = 0
         with open(c[0] + ".h3", "rb") as h3:
-            content_hash.update(h3.read())
-
-    if c[4] == content_hash.digest():
-        print("{} Hash valid".format("Content" if c[2] & 0x2 else "H3"))
+            h3_hashes = h3.read()
+        if SHA1.new(h3_hashes).digest() == c[4]:
+            print("H3 Hash valid")
+        else:
+            print("H3 Hash mismatch!")
+            print(" > TMD:    " + binascii.hexlify(c[4]).decode('utf-8').upper())
+            print(" > Result: " + content_hash.hexdigest().upper())
+        print("Chunks: {}".format(chunk_count))
+        h0_hash_num = 0
+        h1_hash_num = 0
+        h2_hash_num = 0
+        h3_hash_num = 0
+        with open(c[0] + ".app", "rb") as encrypted:
+            with open(c[0] + ".app.dec", "wb") as decrypted:
+                for chunk_num in range(chunk_count):
+                    show_chunk(chunk_num, chunk_count)
+                    # decrypt and verify hash tree
+                    cipher_hash_tree = AES.new(decrypted_titlekey, AES.MODE_CBC, (b"\0" * 16))
+                    hash_tree = cipher_hash_tree.decrypt(encrypted.read(0x400))
+                    h0_hashes = hash_tree[0:0x140]
+                    h1_hashes = hash_tree[0x140:0x280]
+                    h2_hashes = hash_tree[0x280:0x3c0]
+                    h0_hash = h0_hashes[(h0_hash_num * 0x14):((h0_hash_num + 1) * 0x14)]
+                    h1_hash = h1_hashes[(h1_hash_num * 0x14):((h1_hash_num + 1) * 0x14)]
+                    h2_hash = h2_hashes[(h2_hash_num * 0x14):((h2_hash_num + 1) * 0x14)]
+                    h3_hash = h3_hashes[(h3_hash_num * 0x14):((h3_hash_num + 1) * 0x14)]
+                    if SHA1.new(h0_hashes).digest() != h1_hash:
+                        print("\rH0 Hashes invalid in chunk {}".format(chunk_num))
+                    if SHA1.new(h1_hashes).digest() != h2_hash:
+                        print("\rH1 Hashes invalid in chunk {}".format(chunk_num))
+                    if SHA1.new(h2_hashes).digest() != h3_hash:
+                        print("\rH2 Hashes invalid in chunk {}".format(chunk_num))
+                    # doesn't feel right, but only way for a long number I know of
+                    iv = int.from_bytes(h0_hash, byteorder='big') // 0x100000000
+                    #print("{:0>32X}".format(iv))
+                    cipher_content = AES.new(decrypted_titlekey, AES.MODE_CBC, binascii.unhexlify("{:0>32X}".format(iv)))
+                    decrypted_data = cipher_content.decrypt(encrypted.read(0xFC00))
+                    if SHA1.new(decrypted_data).digest() != h0_hash:
+                        print("\rData block hash invalid in chunk {}".format(chunk_num))
+                    decrypted.write(hash_tree + decrypted_data)
+                    h0_hash_num += 1
+                    if h0_hash_num >= 16:
+                        h0_hash_num = 0
+                        h1_hash_num += 1
+                    if h1_hash_num >= 16:
+                        h1_hash_num = 0
+                        h2_hash_num += 1
+                    if h2_hash_num >= 16:
+                        h2_hash_num = 0
+                        h3_hash_num += 1
+                print("")
     else:
-        print("Hash mismatch!")
-        print(" > TMD:    " + binascii.hexlify(c[4]).decode('utf-8').upper())
-        print(" > Result: " + content_hash.hexdigest().upper())
+        cipher_content = AES.new(decrypted_titlekey, AES.MODE_CBC, c[1] + (b"\0" * 14))
+        content_hash = SHA1.new()
+        with open(c[0] + ".app", "rb") as encrypted:
+            with open(c[0] + ".app.dec", "wb") as decrypted:
+                for __ in range(int(math.floor((c[3] / readsize)) + 1)):
+                    to_read = min(readsize, left)
+                    encrypted_content = encrypted.read(to_read)
+                    decrypted_content = cipher_content.decrypt(encrypted_content)
+                    if not c[2] & 0x2:
+                        content_hash.update(decrypted_content)
+                    decrypted.write(decrypted_content)
+                    left -= readsize
+                    show_progress(c[3] - left, c[3])
+                    if left <= 0:
+                        print("")
+                        break
+        if c[4] == content_hash.digest():
+            print("Content Hash valid")
+        else:
+            print("Content Hash mismatch!")
+            print(" > TMD:    " + binascii.hexlify(c[4]).decode('utf-8').upper())
+            print(" > Result: " + content_hash.hexdigest().upper())
